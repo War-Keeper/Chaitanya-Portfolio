2a14ef29b0dfa04e952f1a951d8daf923483dea2
War-Keeper <87688584+War-Keeper@users.noreply.github.com>
2023-08-04 14:04:21 -0400

    nm

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ ghpages.publish(
     'public', // path to public directory
     {
         branch: 'gh-pages',
-        repo: 'https://war-keeper.github.io/Chaitanya-Portfolio/', // Update to point to your repository  
+        repo: 'https://github.com/War-Keeper/Chaitanya-Portfolio.git', // Update to point to your repository  
         user: {
             name: 'Chaitanya Patel', // update to use your name
             email: 'thechikkipatel@gmail.com' // Update to use your email
